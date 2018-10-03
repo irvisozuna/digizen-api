4b39ecf12590a3aa3978551eb690cfb2360c0a73
irvis <irvis.isaac@gmail.com>
2018-10-03 12:54:52 -0700

    Rutas causan problemas

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -5,5 +5,5 @@
  * Date: 09/08/2018
  * Time: 01:09 PM
  */
-Route::get('digizen/upload', 'IrvisOzuna\Digizen\Controllers\MainController@upload');
-Route::get('digizen/index', 'IrvisOzuna\Digizen\Controllers\MainController@index');
+//Route::get('digizen/upload', 'IrvisOzuna\Digizen\Controllers\MainController@upload');
+//Route::get('digizen/index', 'IrvisOzuna\Digizen\Controllers\MainController@index');
